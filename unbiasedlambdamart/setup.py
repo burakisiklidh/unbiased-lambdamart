b66a81570508dbe76c4ac0fec41899497d022059
Burak Isikli <burak.isikli@deliveryhero.com>
2023-01-16 10:53:52 +0100

    fixed openmp

--- unbiasedlambdamart/setup.py
+++ unbiasedlambdamart/setup.py
@@ -7,8 +7,10 @@ exts = [Extension(name="lambdaobj",
                   sources=["lambdaobj.pyx"],
                   libraries=["argsort"],
                   library_dirs=["."],
-                  extra_compile_args=["-fopenmp", "-stdlib=libc++"],
-                  extra_link_args=["-lomp", "-L/usr/local/opt/libomp/lib/", "-stdlib=libc++"]
+                  #extra_compile_args=["-fopenmp", "-stdlib=libc++"],
+                  extra_compile_args=["-fopenmp", "-I/usr/lib/gcc/x86_64-linux-gnu/9/include/"],
+                  #extra_link_args=["-lomp", "-L/usr/local/opt/libomp/lib/", "-stdlib=libc++"]
+                  extra_link_args=["-lstdc++", "-fopenmp", "-L/usr/lib/gcc/x86_64-linux-gnu/9/include"]
                   )]
 
 setup(ext_modules=cythonize(exts))
