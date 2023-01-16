612ce641e197fbea33da87976f461db1d06a5ffc
Burak Isikli <burak.isikli@deliveryhero.com>
2023-01-16 10:44:13 +0100

    updated setup

--- unbiasedlambdamart/build.sh
+++ unbiasedlambdamart/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-export CC=/usr/local/opt/llvm/bin/clang++
+#export CC=/usr/local/opt/llvm/bin/clang++
+export CC=/usr/bin/g++
 
 if [ -f "$CC" ]; then
-	"$CC" -c argsort.cpp
+	"$CC" -c -fPIC argsort.cpp
 	ar rcs libargsort.a argsort.o
 	rm argsort.o
-	/Users/akurennoy/anaconda3/bin/python setup.py build_ext --inplace
+	python3 setup.py build_ext --inplace
 	rm lambdaobj.c libargsort.a
 	rm -rf build
 else
